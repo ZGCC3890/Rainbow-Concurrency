66d964857ad918560a51ea6fa2398344118405d1
zgcc <xgt3890@sina.cn>
2024-12-13 13:07:35 +0800

    导入naive

---
+++ build/CMakeFiles/naive.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/naive.dir/main.cpp.obj"
+  "CMakeFiles/naive.dir/main.cpp.obj.d"
+  "CMakeFiles/naive.dir/utils.cpp.obj"
+  "CMakeFiles/naive.dir/utils.cpp.obj.d"
+  "libnaive.dll.a"
+  "naive.exe"
+  "naive.exe.manifest"
+  "naive.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/naive.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
